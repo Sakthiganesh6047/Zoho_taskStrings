7d0c62d31539dd505cb26f050df773245b926bf5
Sakthiganesh6047 <163615529+Sakthiganesh6047@users.noreply.github.com>
2024-10-05 03:21:14 +0530

    Create InvalidInputException.java

---
+++ InvalidInputException.java
@@ -0,0 +1,11 @@
+package com.invalidinputexception;
+
+public class InvalidInputException extends Exception{
+    public InvalidInputException() {
+        super("Invalid input provided");
+    }
+	
+    public InvalidInputException(String message){
+        super(message);
+    }
+}
